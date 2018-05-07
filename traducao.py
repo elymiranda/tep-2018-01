5150a62853b27bbd547abdf33fa968ab6ed83676
ely.miranda <elydasilvamiranda@gmail.com>
2018-05-07 16:22:20 -0300

    uso básico da requests

--- traducao.py
+++ traducao.py
@@ -1,6 +1,22 @@
-import requests
-import json
-url = "https://api.vagalume.com.br/search.artmus?q=skank%20Vamos%20Fugir&limit=5"
+# coding: utf-8
+import requests, json
+key = 'sua chave'
+q = 'Prezados, boa tarde, gerem uma chave para a API.'
+target = 'en'
 
-request = requests.get(url)
-print(request.json())
+url = 'https://translation.googleapis.com/language/translate/v2'
+params = {'key':key, 'q':q, 'target':target}
+
+response = requests.get(url, params=params)
+'''
+print(response.json())
+{'data': {'translations': [{'detectedSourceLanguage': 'pt',
+    'translatedText': 'Dear, good afternoon, generate a key to the API..'}]}}
+'''
+
+json = response.json()
+print(json['data']['translations'][0]['translatedText'])
+
+
+
+
